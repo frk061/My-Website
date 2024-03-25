22099ad38fda8293a4b9ce99d8a1a8c80c4e7f08
frk061 <141912765+frk061@users.noreply.github.com>
2024-03-25 14:50:39 +0100

    Update mywebsite.py

--- mywebsite.py
+++ mywebsite.py
@@ -13,6 +13,46 @@ def other_page():
 @app.route('/goback')
 def go_back():
     return render_template('aufbau.html.jinja')
-    
+
+@app.route('/peaky')
+def peaky():
+    return render_template('peaky.html.jinja')
+
+@app.route('/When')
+def When():
+    return render_template('When.html.jinja')
+
+@app.route('/Attack')
+def Attack():
+    return render_template('Attack.html.jinja')
+
+@app.route('/sherlock')
+def sherlock():
+    return render_template('sherlock.html.jinja')
+
+@app.route('/Note')
+def Note():
+    return render_template('Note.html.jinja')
+
+@app.route('/matrix')
+def matrix():
+    return render_template('matrix.html.jinja')
+
+@app.route('/interstellar')
+def interstellar():
+    return render_template('interstellar.html.jinja')
+
+@app.route('/harry')
+def harry():
+    return render_template('harry.html.jinja')
+
+@app.route('/dark')
+def dark():
+    return render_template('darkc.html.jinja')
+
+@app.route('/dune')
+def dune():
+    return render_template('dune.html.jinja')
+
 if __name__ == '__main__':
     app.run(debug=True)
